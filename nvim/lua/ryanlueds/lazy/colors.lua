beb825324c9bf22e70009018566cc8864fdb98c7
Ryan Lueder <lueder.ryan@gmail.com>
2024-12-29 21:57:19 -0500

    biannually messing things up

--- nvim/lua/ryanlueds/lazy/colors.lua
+++ nvim/lua/ryanlueds/lazy/colors.lua
@@ -5,6 +5,40 @@ end
 
 return {
     -- {
+    --     "ellisonleao/gruvbox.nvim",
+    --     priority = 1000,
+    --     config = function()
+    --         -- Example config in lua
+    --         --
+    --         -- Default options:
+    --         require("gruvbox").setup({
+    --           terminal_colors = true, -- add neovim terminal colors
+    --           undercurl = false,
+    --           underline = false,
+    --           bold = false,
+    --           italic = {
+    --             strings = false,
+    --             emphasis = false,
+    --             comments = false,
+    --             operators = false,
+    --             folds = false,
+    --           },
+    --           strikethrough = false,
+    --           invert_selection = false,
+    --           invert_signs = false,
+    --           invert_tabline = false,
+    --           invert_intend_guides = false,
+    --           inverse = true, -- invert background for search, diffs, statuslines and errors
+    --           contrast = "", -- can be "hard", "soft" or empty string
+    --           palette_overrides = {},
+    --           overrides = {},
+    --           dim_inactive = false,
+    --           transparent_mode = false,
+    --         })
+    --         vim.cmd("colorscheme gruvbox")
+    --     end,
+    -- },
+    -- {
     --     'shaunsingh/nord.nvim',
     --     lazy = false,
     --     priority = 1000,
@@ -32,19 +66,24 @@ return {
     --         vim.keymap.set('n', '<leader>bg', toggle_transparency, { noremap = true, silent = true })
     --     end,
     -- },
-    -- {
-    --     "catppuccin/nvim",
-    --     name = "catppuccin",
-    --     priority = 1000,
-    --     config = function()
-    --         require("catppuccin").setup({
-    --             flavour = "frappe",
-    --             transparent_background = true,
-    --             term_colors = true,
-    --         })
-    --         vim.cmd.colorscheme('catppuccin')
-    --     end,
-    -- }
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "frappe",
+                transparent_background = true,
+                term_colors = true,
+            })
+            vim.cmd.colorscheme('catppuccin')
+
+
+            vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#CD9EFC' })
+            vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#BABBF6' })
+            vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#BABBF6' })
+        end,
+    }
     -- {
     --   'sainnhe/sonokai',
     --   lazy = false,
@@ -114,30 +153,30 @@ return {
     --         vim.cmd('lua ColorMyPencils()')
     --     end,
     -- },
-    {
-        'thimc/gruber-darker.nvim',
-        config = function()
-            require('gruber-darker').setup({
-                -- OPTIONAL
-                transparent = true, -- removes the background
-                -- underline = false, -- disables underline fonts
-                -- bold = false, -- disables bold fonts
-            })
-            vim.cmd.colorscheme('gruber-darker')
-            vim.cmd('lua TransparentPencils()')
+    -- {
+    --     'thimc/gruber-darker.nvim',
+    --     config = function()
+    --         require('gruber-darker').setup({
+    --             -- OPTIONAL
+    --             transparent = true, -- removes the background
+    --             -- underline = false, -- disables underline fonts
+    --             -- bold = false, -- disables bold fonts
+    --         })
+    --         vim.cmd.colorscheme('gruber-darker')
+    --         vim.cmd('lua TransparentPencils()')
 
-            vim.api.nvim_set_hl(0, 'Type', { fg = '#c4c9c3' })
-            vim.api.nvim_set_hl(0, 'Comment', { fg = '#db9342' })
-            vim.api.nvim_set_hl(0, 'Function', { fg = '#a5b0c7' })
-            vim.api.nvim_set_hl(0, 'Constant', { fg = '#ffffff' })
-            vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#ffdd33' })
-            vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'white' })
-            vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'white' })
-            vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#373737' })
-            -- '#db9342'
-            -- '#ffdd33'  this is the cool yellow color
-        end,
-    },
+    --         -- vim.api.nvim_set_hl(0, 'Type', { fg = '#c4c9c3' })
+    --         -- vim.api.nvim_set_hl(0, 'Comment', { fg = '#db9342' })
+    --         -- vim.api.nvim_set_hl(0, 'Function', { fg = '#a5b0c7' })
+    --         -- vim.api.nvim_set_hl(0, 'Constant', { fg = '#ffffff' })
+    --         vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#ffdd33' })
+    --         vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'white' })
+    --         vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'white' })
+    --         vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#373737' })
+    --         -- '#db9342'
+    --         -- '#ffdd33'  this is the cool yellow color
+    --     end,
+    -- },
     -- {
         --     'behemothbucket/gruber-darker-theme.nvim',
         --     config = function()
--- nvim/lua/ryanlueds/set.lua
+++ nvim/lua/ryanlueds/set.lua
@@ -5,6 +5,8 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+vim.opt.showmode = false
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
